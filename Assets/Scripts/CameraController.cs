3f67a2b5346725f531a91affa37697873042eee8
zelconzeta <126567566+zelconzeta@users.noreply.github.com>
2023-06-03 20:43:40 +0200

    Sábado 03/06/2023 20:42
    He eliminado el paralax, he añadido que le acompañase la cámara y he puesto limites en la escena

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; //este es el objeto que vamos a seguir,o sea el jugador
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x,transform.position.y, transform.position.z);
+    }
+}
