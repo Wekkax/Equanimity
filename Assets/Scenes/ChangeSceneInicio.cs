553c5acf4fa4ab29566fb93e6020670ab109acd8
zelconzeta <126567566+zelconzeta@users.noreply.github.com>
2023-06-05 23:42:19 +0200

    Way to boss avances
    Avances en el way to boss
    - Rehecho el nivel
    - Colocadas Khali y Torvi
    -Algunos colliders
    - Camara following
    - No me acuerdo de que mas :v

--- Assets/Scenes/ChangeSceneInicio.cs
+++ Assets/Scenes/ChangeSceneInicio.cs
@@ -5,25 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneInicio : MonoBehaviour
 {
+    
+    public Vector3 desiredPosition = new Vector3(147.7f, -49.2f, 0f);
 
 
     // Start is called before the first frame update
     void Start()
-    {
-
-
-    }
-
-    
+    {}
 
     private void OnTriggerEnter2D(Collider2D collision)
-
-    {
+        {
+  
         if (collision.gameObject.tag == "Player") //una funcion que solo se cumple si quien entra en el collider tiene tag de Player
         {
             //cambio de escena
             SceneManager.LoadScene("SampleScene 1");
-        }
-    }
 
-}
+          transform.position = desiredPosition;
+           
+        //transform.position = new Vector3(147.7f, -49.2f, 0f);
+                 }
+            
+        }
+  }
+       
+
+       
+
+
+
--- Assets/Scenes/ChangeSceneWayToBoss.cs
+++ Assets/Scenes/ChangeSceneWayToBoss.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeSceneWayToBoss : MonoBehaviour
 {
+    
+    public Vector3 desiredPosition = new Vector3(147.7f, -49.2f, 0f);
 
 
     // Start is called before the first frame update
     void Start()
-    {
-
-
-    }
-
-
+    {}
 
     private void OnTriggerEnter2D(Collider2D collision)
-
-    {
+        {
+  
         if (collision.gameObject.tag == "Player") //una funcion que solo se cumple si quien entra en el collider tiene tag de Player
         {
             //cambio de escena
-            SceneManager.LoadScene("s02_cueva");
-        }
-    }
+            SceneManager.LoadScene("s03_way_to_boss");
 
-}
+          transform.position = desiredPosition;
+           
+        //transform.position = new Vector3(147.7f, -49.2f, 0f);
+                 }
+            
+        }
+  }
+       
+
+       
+
+
+
